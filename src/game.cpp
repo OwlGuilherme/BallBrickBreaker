9fa9a135da0206590b6f75dc8acddb1838ae83f9
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-05-01 12:50:27 -0300

    Desenvolvida função que cria um círculo na tela

---
+++ src/game.cpp
@@ -0,0 +1,22 @@
+#include "../include/game.hpp"
+#include <cmath>
+#include <SDL2/SDL_opengl.h>
+#include <cstddef>
+#include <vector>
+
+void drawCircles(const std::vector<Circle>& circles, int segments) {
+  for (const auto& circle : circles) {
+    glBegin(GL_TRIANGLE_FAN);
+
+    glVertex2f(circle.x, circle.y); // Centro do círculo
+    
+    for (int i = 0; i <= segments; i++) {
+      float angle = 2.0f * M_PI * float(i) / float(segments);
+      float dx = circle.radius * cosf(angle);
+      float dy = circle.radius * sinf(angle);
+      glVertex2f(circle.x + dx, circle.y + dy);
+    }
+
+    glEnd();
+  }
+}
