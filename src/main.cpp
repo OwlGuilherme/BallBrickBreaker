3ec6591b7b1a86df2bfcd53229b5b40b9d66054f
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-05-01 12:50:01 -0300

    Implementa função que gera um círculo na tela

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_opengl.h>
 #include <iostream>
+#include <vector>
+#include "../include/game.hpp"
 
 int main()
 {
@@ -15,8 +17,8 @@ int main()
   }
   
   // Define o tamanho de x e de y em variáveis
-  auto x_size = 600;
-  auto y_size = 400;
+  auto x_size = 500;
+  auto y_size = 600;
 
   SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
@@ -61,10 +63,11 @@ int main()
   auto running = true;
   SDL_Event event;
 
-  auto person_x = 300;
-  auto person_y = 350;
-  auto person_comp = 50;
-  auto person_alt = 30;
+  Person person;
+  person.x = 300;
+  person.y = 350;
+  person.width = 50;
+  person.height = 30;
 
   auto esq = false;
   auto dir = false;
@@ -100,40 +103,51 @@ int main()
           dir = false;
         }
       }
-    }
+    }   
 
-    // Lógica
-    // Movimentos
+    // Movimentação do usuário
     auto vel = 8;
     if (esq == true)
     {
-      if (person_x >= 0)
+      if (person.x >= 0)
       {
-        person_x -= vel;
+        person.x -= vel;
       }      
     }
     if (dir == true)
     {
-      if (person_x + person_comp <= x_size)
+      if (person.x + person.width <= x_size)
       {
-        person_x += vel;
+        person.x += vel;
       }
     }
     glClear(GL_COLOR_BUFFER_BIT); // Limpa o buffer
 
     glPushMatrix(); // Inicialização da matriz
 
-    glOrtho(0, x_size, y_size, 0, -1, 1); // Domensões da matriz
+    glOrtho(0, x_size, y_size, 0, -1, 1); // Dimensões da matriz
     glColor4ub(255, 0, 0, 255);
 
     // Definições do quadrado inferior
     glBegin(GL_QUADS);
-    glVertex2f(person_x, person_y);
-    glVertex2f(person_x + person_comp, person_y);
+    glVertex2f(person.x, person.y);
+    glVertex2f(person.x + person.width, person.y);
     glColor4ub(200, 125, 55, 230);
-    glVertex2f(person_x + person_comp, person_y + person_alt);
-    glVertex2f(person_x, person_y + person_alt);
+    glVertex2f(person.x + person.width, person.y + person.height);
+    glVertex2f(person.x, person.y + person.height);
     glEnd();
+
+    // Desenha círculo na tela
+    std::vector<Circle> circles;
+
+    Circle circle1;
+    circle1.x = 300;
+    circle1.y = 200;
+    circle1.radius = 15;
+    circles.push_back(circle1);
+
+    drawCircles(circles, 10);
+
 
     glPopMatrix(); // Encerramento da matrix
 
